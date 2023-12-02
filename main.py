dc275470039b572eb6c6b5ab7f50e4b74c1e58e8
Evan Mann <ejmann900@gmail.com>
2023-12-02 14:35:04 -0700

    Beginning of the main function created and consols

---
+++ main.py
@@ -0,0 +1,55 @@
+import mysql.connector
+
+def admin_consol():
+    pass
+def data_entry_consol():
+    pass
+def guest_consol():
+    pass
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    #Inital setup
+    print()
+    print("~~~Welcome to the ENSF 300 Museum Database~~~")
+    print()
+    print("Please input your role from the following:\n(1) Database Admin\n(2) Data Entry\n(3) Browse as a Guest")
+    while True:
+        selection = input("Type 1, 2, or 3 for your selection: ")
+        if selection not  in ['1','2','3']:
+            print("Invalid Selection\n")
+        else:
+            break
+
+    if selection in ['1', '2']:
+        username = input("Username: ")
+        password = input("Password: ")
+    else:
+        username = "guest"
+        password = None
+
+    #Uncomment once the database is complete and can actually be used for now this is just a placeholder till later
+
+    #cnx = mysql.connector.connect(
+        #host="127.0.0.1",
+        #port=3306,
+        #user= username,
+        #password= password)    
+    ### Get a cursor
+    #cur = cnx.cursor()
+    ### Execute a query
+    #cur.execute("use olympicarchery")
+    
+    if selection == '1':
+        admin_consol()
+    elif selection == '2':
+        data_entry_consol()
+    else:
+        guest_consol()
