569011f626f5465b266668f4bc39559fc21d6abc
Evan Mann <ejmann900@gmail.com>
2023-12-03 20:40:25 -0700

    added database creation python script

---
+++ dataBaseCreation.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user='root',
+    password= '') #Input local system password
+
+
+# Get a cursor
+cur = cnx.cursor()
+
+fd = open('sql_scripts\museum_db_initialization_WC.sql', 'r')
+sqlFile = fd.read()
+fd.close()
+sqlCommands = sqlFile.split(';')
+
+msg = IOError
+for command in sqlCommands:
+    try:
+        if command.strip() != '':
+            cur.execute(command)
+    except (IOError, msg):
+        print("Command skipped: ", msg)
